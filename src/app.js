e2ee1d792fa95d4bd8d47819e6f02543173c828c
idmacleod <idmacleod@gmail.com>
2020-05-12 19:33:34 +0100

    User can convert from another currency to EUR - MVP complete

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
             fx: {},
             amount: null,
             currency1: "EUR",
-            currency2: "CAD"
+            currency2: "EUR"
         },
         methods: {
             fetchFx: function () {
@@ -23,13 +23,13 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         computed: {
             fxRate: function () {
-                if (this.currency1 === "EUR") {
-                    return this.fx.rates[this.currency2];
-                }
+                if (this.currency1 === this.currency2 ) { return 1; }
+                if (this.currency1 === "EUR") { return this.fx.rates[this.currency2]; }
+                if (this.currency2 === "EUR") { return 1 / this.fx.rates[this.currency1]; }
             },
             convertedAmount: function () {
                 if (this.amount) {
-                    return this.amount * this.fxRate;
+                    return (this.amount * this.fxRate).toFixed(2);
                 }
             }
         }
