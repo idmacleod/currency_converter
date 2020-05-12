86710dd312d704431e9e2a5b90ac1d372789bce0
idmacleod <idmacleod@gmail.com>
2020-05-12 17:57:35 +0100

    Added fetchFx() method and called it in mounted hook

--- src/app.js
+++ src/app.js
@@ -4,7 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: '#app',
         data: {
-            appName: "vueFX"
+            appName: "vueFX",
+            fx: {}
+        },
+        methods: {
+            fetchFx: function () {
+                fetch('https://api.exchangeratesapi.io/latest')
+                    .then(response => response.json())
+                    .then(data => this.fx = data)
+                    .catch(error => console.log(error));
+            }
+        },
+        mounted: function () {
+            this.fetchFx();
         }
     })
 })
