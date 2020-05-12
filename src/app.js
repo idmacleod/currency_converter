1d851f2b23db45c8fd6b5db271c9be5147f345e8
idmacleod <idmacleod@gmail.com>
2020-05-12 20:35:25 +0100

    Adding Vue filter for financial values

--- src/app.js
+++ src/app.js
@@ -4,9 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: '#app',
         data: {
-            appName: "vueFX",
             fx: {},
-            amount: null,
+            amount: 1,
             currency1: "EUR",
             currency2: "EUR"
         },
@@ -26,11 +25,18 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (this.currency1 === this.currency2 ) { return 1; }
                 if (this.currency1 === "EUR") { return this.fx.rates[this.currency2]; }
                 if (this.currency2 === "EUR") { return 1 / this.fx.rates[this.currency1]; }
+                return this.fx.rates[this.currency2] / this.fx.rates[this.currency1];
             },
             convertedAmount: function () {
                 if (this.amount) {
-                    return (this.amount * this.fxRate).toFixed(2);
+                    return this.amount * this.fxRate;
                 }
+            }
+        },
+        filters: {
+            financial: function (number) {
+                if (!number) { return "0.00"; }
+                return number.toFixed(2);
             }
         }
     })
